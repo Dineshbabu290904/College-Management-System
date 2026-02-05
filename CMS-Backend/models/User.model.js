a8bda9759e010c6959b33d57368013529e7eb09e
SDB-DAZN <surapaneni.dinesh@ext.dazn.com>
2026-02-05 19:37:24 +0530

    refactor: simplify password hashing middleware in User model

--- CMS-Backend/models/User.model.js
+++ CMS-Backend/models/User.model.js
@@ -174,10 +174,9 @@ userSchema.index({ branch: 1, semester: 1 });
 userSchema.index({ department: 1 });
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 // Compare password method
